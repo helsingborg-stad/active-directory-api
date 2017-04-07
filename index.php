56bddf269a0a5a8d8b9218dc719f538973027f20
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-04-07 13:43:27 +0200

    Configuration init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once('App.php');
+
+    $api = new \AdApi\App(readfile('configuration.json'));
