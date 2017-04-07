2e26f7ce49b723439bf1c8571d2eb743b9c5df4f
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-04-07 14:58:45 +0200

    Updated configuration

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     require_once('App.php');
+    require_once('configuration.php');
 
     $api =  new \AdApi\App(
-        json_decode(file_get_contents('configuration.json'))
+        $configuration
     );
