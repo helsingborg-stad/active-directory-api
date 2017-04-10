5ad9db6a9e0d7e69cdfbb7c24de06656a4a92972
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-04-10 16:52:45 +0200

    Configuration example.

---
+++ configuration-example.php
@@ -0,0 +1,10 @@
+<?php
+
+    $configuration = array (
+        'base_dn' => 'OU=Adminview,DC=xyz,DC=xyz,DC=se',
+        'account_suffix' => '@xyz.xyz.se',
+        'host' => '127.0.0.0',
+        'port' => 389,
+        'use_tls' => true,
+        'network_timeout' => 5
+    );
