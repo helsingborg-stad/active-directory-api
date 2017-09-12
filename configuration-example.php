fa95b6969e97c454355ca4e3d8c749a40918e396
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-09-12 12:33:24 +0200

    Configuration example

--- configuration-example.php
+++ configuration-example.php
@@ -6,5 +6,12 @@
         'host' => '127.0.0.0',
         'port' => 389,
         'use_tls' => true,
-        'network_timeout' => 5
+        'network_timeout' => 5,
+        'invalid_username_patterns' => array(
+            '/^s([a-z]{4})([0-9]{4})/i',
+            '/^([a-z]{3})([0-9]{4})/i',
+            '/^([a-z]{6})([0-9]{4})/i',
+            '/^([0-9]{6})([a-z]{2})/i',
+            '/adintegration/i', // You want to disable integration etc from bulk import
+        )
     );
